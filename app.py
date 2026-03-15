19439e806616a08fbf1a3ef5f4fec9d3fd2edfca
sujalpanchal-25 <sujalpanchal257@gmail.com>
2026-03-15 13:05:30 +0530

    retest

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    password = "admin123"   # hardcoded secret
-    os.system("ls")         # dangerous command
-    return "Hello SecureFlow"
-    
-app.run(debug=True)
+    return "Secure version"
+
+if __name__ == "__main__":
+    app.run()
